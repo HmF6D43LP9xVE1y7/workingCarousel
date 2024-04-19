6e967fd36caf50fc842a724ace36598b87a94d3e
HmF6D43LP9xVE1y7 <info@timott.ch>
2024-04-19 16:14:01 +0200

    000

---
+++ js/carouselIndividual.js
@@ -0,0 +1,30 @@
+var carousel = new Carousel({
+    elem: 'carouselOne', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 0, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carouselTwo', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 1, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval
+    autoplay: false, // autoplay
+})
+var carousel = new Carousel({
+    elem: 'carouselThree', // target
+    dots: true, // show nav dots
+    arrows: true, // show nav arrows
+    infinite: true, // is infinite
+    initial: 2, // initial slides
+    buttons: false, // autoplay buttons
+    interval: 2000, // autplay interval
+    autoplay: false, // autoplay
+})
